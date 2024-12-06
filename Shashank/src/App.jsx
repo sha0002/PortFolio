bb69360bea8fc8103d2180a0d373a1728b4bbd89
sha0002 <kshashank960@gmail.com>
2024-12-06 16:16:25 +0530

    NavBar Toggle Fix

--- Shashank/src/App.jsx
+++ Shashank/src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import './Responsive.css'
 import Navbar from './Components/NavBar/Navbar'
 import { Outlet } from 'react-router-dom'
+import Banner_Wrapper from './Components/Banner_wrapper/Banner_Wrapper'
+
 
 
 function App() {
